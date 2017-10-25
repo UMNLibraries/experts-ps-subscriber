71aa09eae12ec05e512ffbed7363669b5be24b4d
David Naughton <naughton@umn.edu>
2017-10-25 10:34:09 -0500

    Fixes some mis-named variables.

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ from logging.handlers import RotatingFileHandler
 from flask import Flask, request, jsonify
 PSSubscriber = Flask(__name__)
 
-@app.route('/persons/', methods=['POST'])
+@PSSubscriber.route('/ps_subscriber/', methods=['POST'])
 def person():
-  app.logger.info('testing...')
-  app.logger.info(request.data.decode('utf-8'))
+  PSSubscriber.logger.info('testing...')
+  PSSubscriber.logger.info(request.data.decode('utf-8'))
   response = jsonify(xmltodict.parse(request.data))
   response.status_code = 201
   return response
 
 if __name__ == '__main__':
-  handler = RotatingFileHandler('persons.log', maxBytes=10000, backupCount=1)
+  handler = RotatingFileHandler('ps_subscriber.log', maxBytes=10000, backupCount=1)
   handler.setLevel(logging.INFO)
-  app.logger.setLevel(logging.INFO)
-  app.logger.addHandler(handler)
-  app.run()
+  PSSubscriber.logger.setLevel(logging.INFO)
+  PSSubscriber.logger.addHandler(handler)
+  PSSubscriber.run()
