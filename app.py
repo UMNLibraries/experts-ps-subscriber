d32ca5739571616212e0d5ecdc75e4971ac28f1b
David Naughton <naughton@umn.edu>
2017-10-25 10:58:25 -0500

    Fixes route and log file paths.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from logging.handlers import RotatingFileHandler
 from flask import Flask, request, jsonify
 PSSubscriber = Flask(__name__)
 
-@PSSubscriber.route('/ps_subscriber/', methods=['POST'])
+@PSSubscriber.route('/', methods=['POST'])
 def person():
   PSSubscriber.logger.info('testing...')
   PSSubscriber.logger.info(request.data.decode('utf-8'))
@@ -13,7 +13,7 @@ def person():
   return response
 
 if __name__ == '__main__':
-  handler = RotatingFileHandler('ps_subscriber.log', maxBytes=10000, backupCount=1)
+  handler = RotatingFileHandler('/swadm/var/www/ps_subscriber/shared/ps_subscriber.log', maxBytes=10000, backupCount=1)
   handler.setLevel(logging.INFO)
   PSSubscriber.logger.setLevel(logging.INFO)
   PSSubscriber.logger.addHandler(handler)
